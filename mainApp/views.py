a0b7f5db319e31851635bf4efe15e3cdf6fdbf00
subwayaddicted <novitsky.vladislav00@gmail.com>
2018-03-17 12:55:48 +0200

    Finished main part

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from pprint import pprint
+from django.shortcuts import render
+
 from .models import Artist, Album, Song
 
 # Create your views here.
